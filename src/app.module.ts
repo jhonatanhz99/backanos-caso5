4a051fb9362e34c5e3017b135bf74f7f27ef5891
NORMAN-ITP <normandaveybravo@gmail.com>
2026-09-07 11:37:51 -0500

    Proyecto API Punto 4 - Norman Junior Bravo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+<<<<<<< HEAD
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,3 +9,27 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+=======
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Project } from './entities/project.entity';
+import { Department } from './entities/department.entity';
+
+import { ProjectsModule } from './modules/projects/projects.module';
+import { DepartmentsModule } from './modules/departments/departments.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Project, Department],
+      synchronize: true,
+    }),
+
+    ProjectsModule,
+    DepartmentsModule,
+  ],
+})
+export class AppModule {}
+>>>>>>> d1a6f28 (Proyecto API Punto 4 - Norman Junior Bravo)
